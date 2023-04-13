7b1ce813dd5ef075c3b44fc2c9a06b4961918017
SeoGyun <kihyun2399@gmail.com>
2023-04-13 16:46:29 +0900

    Add Text File [hw6.py]

---
+++ hw6.py
@@ -0,0 +1,11 @@
+#7주차 개념확인 문제
+for s in range(1,10):
+    for i in range(2,6) :
+        print(f'{i} x {s} = {i*s:2d}', end = '   ');
+    print();
+print();
+for s in range(1,10):
+    for i in range(6,10) :
+        print(f'{i} x {s} = {i*s:2d}', end = '   ')
+    print();
+        
